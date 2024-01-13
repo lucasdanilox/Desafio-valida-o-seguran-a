33b05e8982f481eb385031aa2f370fb4744fd1cc
lucasdanilox <lukastecno@gmail.com>
2024-01-12 21:47:05 -0300

    POST City service

--- src/main/java/com/devsuperior/demo/services/CityService.java
+++ src/main/java/com/devsuperior/demo/services/CityService.java
@@ -6,6 +6,8 @@ import com.devsuperior.demo.repositories.CityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -15,9 +17,19 @@ public class CityService {
     @Autowired
     private CityRepository repository;
 
+    @Transactional(readOnly = true)
     public List<CityDTO> findAll() {
         List<City> list = repository.findAll(Sort.by("name"));
         return list.stream().map(x -> new CityDTO(x)).toList();
+
+    }
+
+    @Transactional
+    public CityDTO insert(CityDTO dto) {
+        City entity = new City();
+        entity.setName(dto.getName());
+        entity = repository.save(entity);
+        return new CityDTO(entity);
 
     }
 
